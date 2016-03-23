539e8e07a7d6fadfbf8b819f5287b3ff11070636
Rafael PINTO da SILVA <Rafael PINTO da SILVA>
2016-03-23 08:31:07 +0100

    Envoi de la classe Exceptions

---
+++ src/com/github/rpintodasilva/xebiatondeuse/Exceptions.java
@@ -0,0 +1,10 @@
+package com.github.rpintodasilva.xebiatondeuse;
+
+public class Exceptions {
+	public final static String FILE_NOT_FOUND = "Fichier introuvable.";
+	public final static String FILE_IO_ERROR = "Erreur à la lecture du fichier.";
+	public final static String FILE_TONDEUSE = "Impossible de récupérer des informations pour la tondeuse.";
+	public final static String FILE_DEPLACEMENT = "Impossible de récupérer des informations pour les déplacements.";
+	public final static String FILE_PELOUSE = "Impossible de récupérer des informations pour la pelouse.";
+	
+}
